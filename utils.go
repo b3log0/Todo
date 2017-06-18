5d32abc42835e8d2d21ceb2bf39ae690ad62fdd6
O型血橙 <zephyrjung@126.com>
2017-06-18 22:02:13 +0800

    :construction: working on new issues

--- utils.go
+++ utils.go
@@ -21,3 +21,22 @@ func editDoingFunc(doingFunc func(string,[]string) error,params []string) error 
 	}
 	return nil
 }
+
+func accessGithub(){
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "2612b3f35a40a34b7ecd3506ff37158af93a519c"},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// list all repositories for the authenticated user
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+	if err!=nil {
+		fmt.Println("error")
+	}
+	for _,value := range repos{
+		fmt.Println(value)
+	}
+}
