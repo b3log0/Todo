2a7ed93593b2bd20ea1dd5623ccbaacb5e74e5ef
yu.zhang <zephyrjung@126.com>
2017-09-13 13:46:55 +0800

    :construction: 添加trello模块

---
+++ trello.go
@@ -0,0 +1,98 @@
+package main
+ 
+import (
+    "net/http"
+    "io/ioutil"
+    "fmt"
+    "bytes"
+)
+
+const(
+    API_USER="zephyrcheung"
+    API_KEY=""
+    API_TOKEN=""
+)
+
+func createBoard(board string){
+    api:="https://api.trello.com/1/boards/"
+
+    jsonStr:= []byte("{\"name\":\""+board+"\",\"key\":\""+API_KEY+"\",\"token\":\""+API_TOKEN+"\"}")
+    req,_:=http.NewRequest("POST",api,bytes.NewBuffer(jsonStr))
+    // req.Header.Set("X-Custom-Header", "myvalue")
+    req.Header.Set("Content-Type", "application/json")
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))    
+}
+
+func getGroups(board string){
+    api:="https://api.trello.com/1/boards/"+board+"?fields=id,name,idOrganization,dateLastActivity&lists=open&list_fields=id,name&key="+API_KEY+"&token="+API_TOKEN
+    resp,_ := http.Get(api)
+    defer resp.Body.Close()
+    body,_:=ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body: ",string(body))
+}
+
+func getBoards(){
+    api:="https://api.trello.com/1/members/"+API_USER+"/boards?fields=id,name&key="+API_KEY+"&token="+API_TOKEN
+    resp,_ := http.Get(api)
+    defer resp.Body.Close()
+    body,_:=ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body: ",string(body))
+}
+
+func getCards(board string){
+    api:="https://api.trello.com/1/boards/"+board+"/cards/?fields=id,name,desc,idList&key="+API_KEY+"&token="+API_TOKEN
+    resp,_ := http.Get(api)
+    defer resp.Body.Close()
+    body,_:=ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body: ",string(body))
+}
+
+func createCard(card string,desc string,idList string){
+    api:="https://api.trello.com/1/cards"
+    jsonStr:= []byte("'name':'"+card+"','desc':'"+desc+"','idList':'"+idList+"','key':'"+API_KEY+"','token':'"+API_TOKEN+"'")
+    req,_:=http.NewRequest("POST",api,bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+    client:=&http.Client{}
+    client.Do(req) 
+}
+
+func updateCard(idCard string,card string,desc string,idList string){
+    api:="https://api.trello.com/1/cards/"+idCard
+    jsonStr:= []byte("'name':'"+card+"','desc':'"+desc+"','idList':'"+idList+"','key':'"+API_KEY+"','token':'"+API_TOKEN+"'")
+    req,_:=http.NewRequest("PUT",api,bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+    client:=&http.Client{}
+    client.Do(req) 
+}
+
+func closeCard(idCard string){
+    api:="https://api.trello.com/1/cards/"+idCard
+    jsonStr:= []byte("'closed':true,'key':'"+API_KEY+"','token':'"+API_TOKEN+"'")
+    req,_:=http.NewRequest("PUT",api,bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+    client:=&http.Client{}
+    client.Do(req) 
+}
+
+func closeBoard(idBoard string){
+    api:="https://api.trello.com/1/boards/"+idBoard
+    jsonStr:= []byte("'closed':true,'key':'"+API_KEY+"','token':'"+API_TOKEN+"'")
+    req,_:=http.NewRequest("PUT",api,bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+    client:=&http.Client{}
+    client.Do(req) 
+}
+
+func main() {
+    createBoard("Test By Go2")
+}
