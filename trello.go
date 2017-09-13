b2a0117d44e9ca398f65eee1a09f6cefb1ed7ff8
yu.zhang <zephyrjung@126.com>
2017-09-13 15:48:27 +0800

    :fire: 删除调试输出信息

--- trello.go
+++ trello.go
@@ -3,7 +3,6 @@ package main
 import (
     "net/http"
     "io/ioutil"
-    "fmt"
     "bytes"
 )
 
@@ -18,43 +17,38 @@ func createBoard(board string){
 
     jsonStr:= []byte("{\"name\":\""+board+"\",\"key\":\""+API_KEY+"\",\"token\":\""+API_TOKEN+"\"}")
     req,_:=http.NewRequest("POST",api,bytes.NewBuffer(jsonStr))
-    // req.Header.Set("X-Custom-Header", "myvalue")
     req.Header.Set("Content-Type", "application/json")
     client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
+    client.Do(req)
+    // resp, err := client.Do(req)
+    // if err != nil {
+    //     panic(err)
+    // }
+    // defer resp.Body.Close()
 
-    fmt.Println("response Status:", resp.Status)
-    fmt.Println("response Headers:", resp.Header)
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Println("response Body:", string(body))    
+    // fmt.Println("response Status:", resp.Status)
+    // fmt.Println("response Headers:", resp.Header)
+    // body, _ := ioutil.ReadAll(resp.Body)
+    // fmt.Println("response Body:", string(body))    
 }
 
 func getGroups(board string){
     api:="https://api.trello.com/1/boards/"+board+"?fields=id,name,idOrganization,dateLastActivity&lists=open&list_fields=id,name&key="+API_KEY+"&token="+API_TOKEN
-    resp,_ := http.Get(api)
-    defer resp.Body.Close()
-    body,_:=ioutil.ReadAll(resp.Body)
-    fmt.Println("response Body: ",string(body))
+    http.Get(api)
+    // resp,_ := http.Get(api)
+    // defer resp.Body.Close()
+    // body,_:=ioutil.ReadAll(resp.Body)
+    // fmt.Println("response Body: ",string(body))
 }
 
 func getBoards(){
     api:="https://api.trello.com/1/members/"+API_USER+"/boards?fields=id,name&key="+API_KEY+"&token="+API_TOKEN
-    resp,_ := http.Get(api)
-    defer resp.Body.Close()
-    body,_:=ioutil.ReadAll(resp.Body)
-    fmt.Println("response Body: ",string(body))
+    http.Get(api)
 }
 
 func getCards(board string){
     api:="https://api.trello.com/1/boards/"+board+"/cards/?fields=id,name,desc,idList&key="+API_KEY+"&token="+API_TOKEN
-    resp,_ := http.Get(api)
-    defer resp.Body.Close()
-    body,_:=ioutil.ReadAll(resp.Body)
-    fmt.Println("response Body: ",string(body))
+    http.Get(api)
 }
 
 func createCard(card string,desc string,idList string){
@@ -64,16 +58,7 @@ func createCard(card string,desc string,idList string){
     req,_:=http.NewRequest("POST",api,bytes.NewBuffer(jsonStr))
     req.Header.Set("Content-Type", "application/json")
     client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
-
-    fmt.Println("response Status:", resp.Status)
-    fmt.Println("response Headers:", resp.Header)
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Println("response Body:", string(body))  
+    client.Do(req)
 }
 
 func updateCard(idCard string,card string,desc string,idList string){
@@ -109,16 +94,7 @@ func closeBoard(idBoard string){
     req,_:=http.NewRequest("PUT",api,bytes.NewBuffer(jsonStr))
     req.Header.Set("Content-Type", "application/json")
     client:=&http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
-
-    fmt.Println("response Status:", resp.Status)
-    fmt.Println("response Headers:", resp.Header)
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Println("response Body:", string(body))  
+    client.Do(req)
 }
 
 func openBoard(idBoard string){
